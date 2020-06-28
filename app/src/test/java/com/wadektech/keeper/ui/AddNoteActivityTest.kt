889b644456517bf21b5c63093096ef523c4ff690
Derrick Wadek <wadeq@Derricks-MacBook-Air.local>
2020-06-28 18:50:50 +0300

    tests

--- app/src/test/java/com/wadektech/keeper/ui/AddNoteActivityTest.kt
+++ app/src/test/java/com/wadektech/keeper/ui/AddNoteActivityTest.kt
@@ -1,12 +1,18 @@
 package com.wadektech.keeper.ui
 
+import com.wadektech.keeper.models.Note
 import org.junit.jupiter.api.Test
-
-import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Assertions
 
 internal class AddNoteActivityTest {
 
     @Test
     fun saveNotesToDB() {
+        val title = "Title"
+        val body = "This is a note for testing our note function."
+        val note = Note(title,body)
+        Assertions.assertEquals(title, note.title)
+        Assertions.assertEquals(body, note.note)
+
     }
 }
